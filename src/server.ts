0be9ac95f882bdf2db1048e95af7941030a80843
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-02-22 07:40:09 +0200

    use prettier&lint on reporter.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-import express from 'express'
+import express from 'express';
+const app = express();
+const port = 2784;
+
+app.get('/', (req, res) => {
+  res.send('Hola world! :D');
+});
+
+app.listen(port, () => {
+  console.log(`server is up and running :D on port ${port}`);
+});
