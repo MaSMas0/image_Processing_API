2b917bee5fabce12c9adfcaa422c15c030fe53a1
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-02-23 14:56:19 +0200

    add test file for the utility file & all tests failed :(

---
+++ src/tests/utilitySpec.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+/* the reason is that some modification  of prettier doesn't go well in this part so i let it 
+till some point then i disabled it */
+import * as util from '../utilities/utility';
+
+// create a request object
+
+describe('Image Directory paths', () => {
+  it('check the original images directory absolute paths', () => {
+    expect(util.existingImgPath).toBe(
+      '/Users/mas/Desktop/Image_Processing_API/public/images')
+    })
+
+  it('check the generated images directory absolute paths', () => {
+    expect(util.generatedImgPath).toEqual(
+      '/Users/mas/Desktop/Image_Processing_API/public/thumbs'
+    )
+  });
+});
