84f599af153557cc82acfe1bf7b4dec810da6f53
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-02-24 05:34:07 +0200

    make app module concise and separate caching

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,20 @@
 import sharp from 'sharp';
-import fs from 'fs/promises';
-import * as util from './utilities/utility';
+import { existingImgPath, generatedImgPath } from './utilities/utility';
 
 const imageProcessing = async (
   width: number,
   height: number,
-  mode: string,
   filename: string
 ) => {
-  const existingImageFilePath = `${util.existingImgPath}/${filename}.jpg`;
-  const generatedImageFilePath = `${util.generatedImgPath}/${filename}_${width}_${height}_${mode}.jpg`;
+  const existingImageFilePath = `${existingImgPath}/${filename}.jpg`;
+  const generatedImageFilePath = `${generatedImgPath}/${filename}_${width}_${height}.jpg`;
   try {
-    const processedImage = await sharp(existingImageFilePath)
-      .resize(width, height)
-      .toBuffer();
-    fs.writeFile(generatedImageFilePath, processedImage);
+    await sharp(existingImageFilePath)
+      .resize(Number(width), Number(height))
+      .toFile(generatedImageFilePath);
   } catch (error) {
     throw new Error('Unable to process image');
   }
 };
 
-//caching
-//checking if image exist
-const cachedImage = async (
-  width: number,
-  height: number,
-  mode: string,
-  filename: string
-) => {
-  const generatedImageFilePath = `${util.generatedImgPath}/${filename}_${width}_${height}_${mode}.jpg`;
-  try {
-    await fs.access(generatedImageFilePath);
-    return generatedImageFilePath;
-  } catch (error) {
-    throw new Error('file does not exist');
-  }
-};
-export default { imageProcessing, cachedImage };
+export { imageProcessing };
