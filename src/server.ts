20bea51ce3a6048858b1fed8c5e28db78ffbcee2
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-02-24 07:35:31 +0200

    update server.ts to work ejs and routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes/routes';
+
 const app = express();
 const port = 2784;
 
-app.get('/', (_req, res) => {
-  res.send('Hola world! :D');
+app.use('/images', express.static(path.join(__dirname, '../public/images')));
+app.set('view engine', 'ejs');
+app.use('/magic', routes);
+app.use((req, res) => {
+  res.status(404).send("ops! can't find what you are looking for :(");
 });
-
 app.listen(port, () => {
   console.log(`server is up and running :D on port ${port}`);
 });
